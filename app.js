2ca39450bc509496949b6bbc0c567f21c037978a
Brett Bellomy <brettbellomymusic@gmail.com>
2024-08-20 15:48:51 -0500

    completed assignement

--- app.js
+++ app.js
@@ -3,5 +3,42 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function printOdds(count) {
+    if (count < 0) {
+        console.log("Please enter a number > 0");
+        return;
+    }
+
+    for (let i = 0; i <= count; i++) {
+        if (i % 2 != 0) {
+            console.log(i);
+        }
+    }
+}
+
+printOdds(100);
+printOdds(-100);
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+function checkAge(userName, age) {
+
+    if (!userName || !age) {
+        console.log("Please provide your name and age");
+        return;
+    }
+
+    let aboveSixteen = `Congrats ${userName}, you can drive!`;
+    let belowSixteen = `Sorry ${userName}, but you can't drive. Please check back when you're 16.`;
+
+    if (age < 16) {
+        console.log(belowSixteen);
+    } else {
+        console.log(aboveSixteen);
+    }
+}
+checkAge();
+checkAge(15);
+checkAge("Brett Bellomy", 30);
+checkAge("Naruto Uzumaki", 15);
